f00f5a46583d21d27a6069978c9ac80d8325f3f8
yigarashi <y.igarashi.843@ms.saitama-u.ac.jp>
2023-10-30 13:16:41 +0900

    DQN2成功

---
+++ dqn2.py
@@ -0,0 +1,191 @@
+from copy import copy
+from copy import deepcopy
+import numpy as np
+import matplotlib.pyplot as plt
+from dezero import Model
+from dezero import optimizers
+from dezero import Variable
+import dezero.functions as F
+import dezero.layers as L
+from collections import deque
+import random
+
+from modules import system as s
+
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
+
+    def __len__(self):
+        return len(self.buffer)
+    
+    def get_batch(self):
+        data = random.sample(self.buffer, self.batch_size)
+        state = np.stack([x[0] for x in data])
+        action = np.array([x[1] for x in data])
+        reward = np.array([x[2] for x in data])
+        next_state = np.stack([x[3] for x in data])
+        done = np.array([x[4] for x in data])
+        return state, action, reward, next_state, done
+
+class QNet(Model):
+    def __init__(self):
+        super().__init__()
+        self.l1 = L.Linear(128)
+        self.l2 = L.Linear(128)
+        self.l3 = L.Linear(3)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+
+class DQNAgent:
+    def __init__(self):
+        self.gamma = 0.98
+        self.lr = 0.0005
+        self.buffer_size = 10000
+        self.batch_size = 32
+        self.action_size = 3
+
+        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.qnet = QNet()
+        self.qnet_target = QNet()
+        self.optimizer = optimizers.Adam(self.lr)
+        self.optimizer.setup(self.qnet)
+
+    def sync_qnet(self):
+        self.qnet_target = deepcopy(self.qnet)
+
+    def get_action(self, state, epsilon):
+        if np.random.rand() < epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            state = state[np.newaxis, :]
+            qs = self.qnet(state)
+            return qs.data.argmax()
+
+    def update(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        if len(self.replay_buffer) < self.batch_size:
+            return None
+
+        state, action, reward, next_state, done = self.replay_buffer.get_batch()
+        qs = self.qnet(state)
+        q = qs[np.arange(self.batch_size), action]
+
+        next_qs = self.qnet_target(next_state)
+        next_q = next_qs.max(axis=1)
+        next_q.unchain()
+        target = reward + done * self.gamma * next_q
+
+        loss = F.mean_squared_error(q, target)
+
+        self.qnet.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+
+        return loss.data
+
+
+def main():
+    episodes = 500
+    sync_interval = 20
+
+    dt = 5e-13 # [s]
+    alphaG = 0.01
+    uniaxial_anisotropy = 540e0 # [Oe]
+    dh = 50e0 # [Oe]
+    m0 = np.array([0e0, 0e0, 1e0])
+
+    agent = DQNAgent()
+    reward_history = []
+    high_reward = -6
+    loss_history = []
+
+    for episode in range(episodes):
+        print("episode:{:>4}".format(episode), end=":")
+        dynamics = s.Dynamics(dt, alphaG, uniaxial_anisotropy, m0, limit=2001)
+
+        t = []
+        m = []
+        h = []
+
+        if episode < 100:
+            epsilon = 0.2
+        elif episode < 400:
+            epsilon = 0.1
+        else:
+            epsilon = 0.01
+
+        old_m = np.array([0e0, 0e0, 1e0])
+        old_reward = - old_m[2]
+        total_reward = 0
+        total_loss = 0
+        cnt = 0
+        done = 1
+        field = np.array([0e0, 0e0, 0e0])
+
+        for i in np.arange(dynamics.limit):
+            if i % 400 == 0:
+                action = agent.get_action(dynamics.m, epsilon)
+                h0 = action - 1
+                if i == 0:
+                    old_action = action
+
+            field += np.array([dh*h0*2e-2, 0e0, 0e0])      
+
+            time = i*dt
+
+            dynamics.RungeKutta(field)
+
+            if i % 10 == 0:
+                t.append(time)
+                m.append(dynamics.m)
+                h.append(copy(field))
+
+            if i % 400 == 0:
+                reward = - dynamics.m[2]
+                total_reward += reward
+
+                if i == dynamics.limit - 1:
+                    done = 0
+
+                if i != 0:
+#                    agent.update(old_m, old_action, old_reward, dynamics.m, done)
+                    loss = agent.update(old_m, old_action, old_reward, dynamics.m, done)
+                    if loss is not None:
+                        total_loss += loss
+                        cnt += 1
+
+                    old_m = dynamics.m
+                    old_action = action
+                    old_reward = reward
+
+        if episode % sync_interval == 0:
+            agent.sync_qnet()
+
+        if total_reward > high_reward:
+            s.save_episode(episode, t, m, h, "fig_dqn2")
+            high_reward = total_reward
+
+        print("reward = {:.9f}".format(total_reward))
+
+        reward_history.append(total_reward)
+
+        if loss is not None:
+            ave_loss = total_loss / cnt
+            loss_history.append(ave_loss)
+            s.save_loss_history(loss_history, "fig_dqn2")
+
+        s.save_reward_history(reward_history, "fig_dqn2")
+
+
+if __name__ == '__main__':
+    main()
