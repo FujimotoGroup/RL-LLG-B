f82c151dd288b459bba12c3f2d508c88a2403c1c
yigarashi <y.igarashi.843@ms.saitama-u.ac.jp>
2023-11-03 20:53:41 +0900

    dqn_x

--- modules/system_torch.py
+++ modules/system_torch.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+import torch
 
 class Magnetization:
     def __init__(self, m0:np.array):
@@ -70,59 +71,35 @@ def plot(t:list, m:list, h:list, issave:bool=False):
 
 def save_episode(episode:int, t:list, m:list, h:list, directory:str):
     fig = plot(t,m,h, issave=True)
-    fig.savefig(directory+"/episode{:0=5}.png".format(episode+1), dpi=200)
+    fig.tight_layout()
+    fig.savefig(directory+"/dynamics_episode{:0=5}.png".format(episode), dpi=200)
     plt.close()
 
-def save_reward_history(reward_history:list, directory:str):
-    reward_history_array = np.array(reward_history)
-    episodes = np.arange(len(reward_history))
-
-    slice_num = 10
-    average = [ reward_history_array[i:i+slice_num].mean() for i in episodes[::slice_num]]
-
-    plt.xlabel('Episode')
-    plt.ylabel('Total Reward')
-    plt.plot(range(len(reward_history)), reward_history)
-    plt.plot(episodes[::slice_num], average)
-#    plt.show()
-    plt.savefig(directory+"/reward_history.png", dpi=200)
-    plt.close()
-
-def save_loss_history(loss_history:list, directory:str):
-    plt.xlabel('Episode')
-    plt.ylabel('loss')
-    plt.plot(range(len(loss_history)), loss_history)
-#    plt.show()
-    plt.savefig(directory+"/loss_history.png", dpi=200)
-    plt.close()
-
-def save_loss_pi_history(loss_pi_history:list, directory:str):
-    plt.xlabel('Episode')
-    plt.ylabel('loss_pi')
-    plt.plot(range(len(loss_pi_history)), loss_pi_history)
-#    plt.show()
-    plt.savefig(directory+"/loss_pi_history.png", dpi=200)
-    plt.close()
-
-def save_prob(episode:int, agent, directory:str):
+def save_prob_value(episode:int, agent, directory:str):
     n = agent.action_size
     mesh_num = 20
     theta_u = np.linspace(0e0, np.pi/2e0, mesh_num)
     phi = np.linspace(0e0, 2e0*np.pi, mesh_num)
 #    theta, phi = np.meshgrid(theta, phi)
     probs = []
+    values = []
     for t in theta_u:
         prob = []
+        value = []
         for p in phi:
             x = np.sin(t)*np.cos(p)
             y = np.sin(t)*np.sin(p)
             z = np.cos(t)
             S = np.array([x, y, z])
-            S = S[np.newaxis,:]
-            pi = agent.qnet(S)[0].data
+            S = torch.from_numpy(S[np.newaxis,:].astype(np.float32)).to(agent.device)
+            pi = agent.pi(S)[0].to(torch.device('cpu')).detach().numpy()
+            v = agent.v(S)[0,0].to(torch.device('cpu')).detach().numpy()
             prob.append(pi)
+            value.append(v)
         probs.append(prob)
+        values.append(value)
     probs_u = np.array(probs)
+    values_u = np.array(values)
 
     theta, phi = np.meshgrid(theta_u, phi)
     x_u = np.sin(theta)*np.cos(phi)
@@ -131,46 +108,101 @@ def save_prob(episode:int, agent, directory:str):
     theta_d = np.linspace(np.pi/2e0, np.pi, mesh_num)
     phi = np.linspace(0e0, 2e0*np.pi, mesh_num)
     probs = []
+    values = []
     for t in theta_d:
         prob = []
+        value = []
         for p in phi:
             x = np.sin(t)*np.cos(p)
             y = np.sin(t)*np.sin(p)
             z = np.cos(t)
             S = np.array([x, y, z])
-            S = S[np.newaxis,:]
-            pi = agent.qnet(S)[0].data
+            S = torch.from_numpy(S[np.newaxis,:].astype(np.float32)).to(agent.device)
+            pi = agent.pi(S)[0].to(torch.device('cpu')).detach().numpy()
+            v = agent.v(S)[0,0].to(torch.device('cpu')).detach().numpy()
             prob.append(pi)
+            value.append(v)
         probs.append(prob)
+        values.append(value)
     probs_d = np.array(probs)
+    values_d = np.array(values)
 
     theta, phi = np.meshgrid(theta_d, phi)
     x_d = np.sin(theta)*np.cos(phi)
     y_d = np.sin(theta)*np.sin(phi)
 
     fig, axes = plt.subplots(2, n, figsize=(5*n, 12))
-#    window = 2e0/n
-#    levels = np.linspace(0e0, window, 100)
-#    norm = mpl.colors.Normalize(vmin=0e0, vmax=window)
+    window = 2e0/n
+    levels = np.linspace(0e0, window, 100)
+    norm = mpl.colors.Normalize(vmin=0e0, vmax=window)
     for i in np.arange(n):
         axes[0][i].set_xlim([-1e0, 1e0])
         axes[0][i].set_ylim([-1e0, 1e0])
         axes[0][i].axis('equal')
-#        axes[0][i].contourf(x_u, y_u, probs_u[:,:,i], levels=levels, cmap="bwr", extend="max")
-        ax = axes[0][i].contourf(x_u, y_u, probs_u[:,:,i])
-        cax = make_axes_locatable(axes[0][i]).append_axes("right", size="3%", pad=0.1)
-        fig.colorbar(ax, cax=cax)
+        axes[0][i].contourf(x_u, y_u, probs_u[:,:,i], levels=levels, cmap="bwr", extend="max")
+#        ax = axes[0][i].contourf(x_u, y_u, probs_u[:,:,i])
+#        cax = make_axes_locatable(axes[0][i]).append_axes("right", size="3%", pad=0.1)
+#        fig.colorbar(ax, cax=cax)
 
         axes[1][i].set_xlim([-1e0, 1e0])
         axes[1][i].set_ylim([-1e0, 1e0])
         axes[1][i].axis('equal')
-#        ax = axes[1][i].contourf(x_d, y_d, probs_d[:,:,i], levels=levels, cmap="bwr", extend="max")
-        ax = axes[1][i].contourf(x_d, y_d, probs_d[:,:,i])
-        cax = make_axes_locatable(axes[1][i]).append_axes("right", size="3%", pad=0.1)
-        fig.colorbar(ax, cax=cax)
+        ax = axes[1][i].contourf(x_d, y_d, probs_d[:,:,i], levels=levels, cmap="bwr", extend="max")
+#        ax = axes[1][i].contourf(x_d, y_d, probs_d[:,:,i])
+#        cax = make_axes_locatable(axes[1][i]).append_axes("right", size="3%", pad=0.1)
+#        fig.colorbar(ax, cax=cax)
 
-#    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap="bwr"),ax=axes.ravel().tolist(), extend="max")
+    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap="bwr"),ax=axes.ravel().tolist(), extend="max")
 
 #    fig.tight_layout()
     fig.savefig(directory+"/prob_episode{:0=5}.png".format(episode), dpi=200)
+    plt.close()
+
+    fig, axes = plt.subplots(2, 1, figsize=(6, 12))
+    maximum = max(values_u.max(), values_d.max())
+    levels = np.linspace(0e0, maximum, 200)
+    norm = mpl.colors.Normalize(vmin=0e0, vmax=maximum)
+    axes[0].set_xlim([-1e0, 1e0])
+    axes[0].set_ylim([-1e0, 1e0])
+    axes[0].axis('equal')
+    axes[0].contourf(x_u, y_u, values_u, levels=levels, cmap="Blues", extend="max")
+#    ax = axes[0][i].contourf(x_u, y_u, probs_u[:,:,i])
+#    cax = make_axes_locatable(axes[0][i]).append_axes("right", size="3%", pad=0.1)
+#    fig.colorbar(ax, cax=cax)
+
+    axes[1].set_xlim([-1e0, 1e0])
+    axes[1].set_ylim([-1e0, 1e0])
+    axes[1].axis('equal')
+    ax = axes[1].contourf(x_d, y_d, values_d, levels=levels, cmap="Blues", extend="max")
+#    ax = axes[1][i].contourf(x_d, y_d, probs_d[:,:,i])
+#    cax = make_axes_locatable(axes[1][i]).append_axes("right", size="3%", pad=0.1)
+#    fig.colorbar(ax, cax=cax)
+
+    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap="Blues"),ax=axes.ravel().tolist(), extend="max")
+
+#    fig.tight_layout()
+    fig.savefig(directory+"/value_episode{:0=5}.png".format(episode), dpi=200)
+    plt.close()
+
+def save_history(reward_history:list, lr_history:list, directory:str):
+    reward_history = np.array(reward_history)
+    lr_history = np.array(lr_history)
+    episodes = np.arange(len(reward_history))
+
+    slice_num = 10
+    average = [ reward_history[i:i+slice_num].mean() for i in episodes[::slice_num]]
+
+    fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+    axes[0].set_xlabel('Episode')
+    axes[0].set_ylabel('Total Reward')
+    axes[0].plot(episodes, reward_history)
+    axes[0].plot(episodes[::slice_num], average)
+
+    axes[1].set_xlabel('Episode')
+    axes[1].set_ylabel('learning rate')
+    axes[1].set_yscale('log')
+    axes[1].plot(episodes, lr_history)
+#    plt.show()
+    fig.tight_layout()
+    fig.savefig(directory+"/history.png", dpi=200)
     plt.close()
